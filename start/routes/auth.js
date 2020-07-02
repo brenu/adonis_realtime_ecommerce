49a6a5fc4bc4ae169370c74d2391f0088f40a040
brenu <bvsousa64@gmail.com>
2020-07-02 18:59:04 -0300

    :construction: Define password reset routes

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -4,10 +4,15 @@
 const Route = use('Route');
 
 Route.group(() => {
-  Route.post('/register', 'AuthController.register').as('auth.register');
-  Route.post('/login', 'AuthController.login').as('auth.login');
-  Route.put('/refresh', 'AuthController.refresh').as('auth.refresh');
-  Route.delete('/logout', 'AuthController.logout').as('auth.logout');
+  Route.post('register', 'AuthController.register').as('auth.register');
+  Route.post('login', 'AuthController.login').as('auth.login');
+  Route.put('refresh', 'AuthController.refresh').as('auth.refresh');
+  Route.delete('logout', 'AuthController.logout').as('auth.logout');
+
+  // Password Reset routes
+  Route.post('password-reset', 'AuthController.forgot').as('auth.forgot');
+  Route.get('password-reset', 'AuthController.remember').as('auth.remember');
+  Route.put('password-reset', 'AuthController.reset').as('auth.reset');
 })
   .prefix('api/v1/auth')
   .namespace('Auth');
