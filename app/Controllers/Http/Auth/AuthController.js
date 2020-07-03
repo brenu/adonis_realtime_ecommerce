8a6909b6028d9dfd0ba0c249b79a8e87f8636c5a
brenu <bvsousa64@gmail.com>
2020-07-03 13:29:35 -0300

    :construction: Implement register method for customers

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,8 +1,38 @@
 'use strict';
 
+const Database = use('Database');
+const User = use('App/Models/User');
+const Role = use('Role');
+
 class AuthController {
   async register({ req, res }) {
-    //
+    const trx = await Database.beginTransaction();
+    try {
+      const { name, surname, email, password } = request.all();
+
+      const user = await User.create(
+        {
+          name,
+          surname,
+          email,
+          password,
+        },
+        trx
+      );
+
+      const userRole = await Role.findBy('slug', 'customer');
+
+      await user.roles().attach([userRole.id], null, trx);
+
+      await trx.commit();
+
+      return res.status(201).send({ data: user });
+    } catch (error) {
+      await trx.rollback();
+      return res.status(400).send({
+        message: 'Erro ao realizar cadastro!',
+      });
+    }
   }
 
   async login({ req, res, auth }) {
