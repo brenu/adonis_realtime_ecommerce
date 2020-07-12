c03eee7b5494fd7138f568cb5cc6d8237079ea50
brenu <bvsousa64@gmail.com>
2020-07-11 21:18:32 -0300

    :construction: Implement show method for admin's user controller

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -69,7 +69,6 @@ class UserController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
   async show({ params: { id }, request, response }) {
     const user = User.findOrFail(id);
