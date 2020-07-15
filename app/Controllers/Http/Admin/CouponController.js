29bdc07086f3e31ea70c5d3ff0538472c44c9f6b
brenu <bvsousa64@gmail.com>
2020-07-15 01:00:19 -0300

    :construction: Implement coupon controller destroy method

--- app/Controllers/Http/Admin/CouponController.js
+++ app/Controllers/Http/Admin/CouponController.js
@@ -5,6 +5,7 @@
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const Coupon = use('App/Models/Coupon');
+const Database = use('Database');
 
 /**
  * Resourceful controller for interacting with coupons
@@ -75,7 +76,26 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const trx = await Database.beginTransaction();
+
+    const coupon = await Coupon.findOrFail(id);
+    try {
+      await coupon.products().detach([], trx);
+      await coupon.orders().detach([], trx);
+      await coupon.users().detach([], trx);
+      await coupon.delete(trx);
+
+      await trx.commit();
+
+      return response.status(204).send({});
+    } catch (error) {
+      await trx.rollback();
+      return response
+        .status(400)
+        .send({ message: 'Não foi possível deletar o cupom' });
+    }
+  }
 }
 
 module.exports = CouponController;
