0fd2303a5d3b99990c2ea93ea0def02e612ac4a5
brenu <bvsousa64@gmail.com>
2020-07-18 20:21:59 -0300

    :construction: Implement calculateValues discount hook

---
+++ app/Models/Hooks/DiscountHook.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Coupon = use('App/Models/Coupon');
+const Order = use('App/Models/Order');
+
+const DiscountHook = (exports = module.exports = {});
+
+DiscountHook.calculateValues = async model => {
+  var couponProducts,
+    discountItems = [];
+  model.discount = 0;
+
+  const coupon = await Coupon.find(model.coupon_id);
+  const order = await Order.find(model.order_id);
+
+  switch (coupon.can_use_for) {
+    case 'product_customer' || 'product':
+      couponProducts = await Database.from('coupon_product')
+        .where('coupon_id', model.coupon_id)
+        .pluck('product_id');
+      discountItems = await Database.from('order_items')
+        .where('order_id', model.order_id)
+        .whereIn('product_id', couponProducts);
+
+      if (coupon.type === 'percent') {
+        for (let orderItem of discountItems) {
+          model.discount += (orderItem.subTotal / 100) * coupon.discount;
+        }
+      } else if (coupon.type === 'currency') {
+        for (let orderItem of discountItems) {
+          model.discount += coupon.discount * orderItem.quantity;
+        }
+      } else {
+        for (let orderItem of discountItems) {
+          model.discount += orderItem.subTotal;
+        }
+      }
+
+      break;
+
+    default:
+      // Cupom pode ser usado só por clientes específicos ou por todo mundo
+      if (coupon.type === 'percent') {
+        model.discount = (order.subTotal / 100) * coupon.discount;
+      } else if (coupon.type === 'currency') {
+        model.discount = coupon.discount;
+      } else {
+        model.discount = orderItem.subTotal;
+      }
+      break;
+  }
+
+  return model;
+};
