593ca40bb3561687eb62178d7bb93a807d94f58e
brenu <bvsousa64@gmail.com>
2020-07-19 11:18:17 -0300

    :construction: Implement order controller applyDiscount method

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -5,6 +5,8 @@
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const Order = use('App/Models/Order');
+const Coupon = use('App/Models/Coupon');
+const Discount = use('App/Models/Discount');
 const Database = use('Database');
 const Service = use('App/Services/Order/OrderService');
 
@@ -143,6 +145,40 @@ class OrderController {
       return response
         .status(400)
         .send({ message: 'Não foi possível deletar o pedido' });
+    }
+  }
+
+  async applyDiscount({ params: { id }, request, response }) {
+    const { code } = request.all();
+    const coupon = await Coupon.findByOrFail('code', code.toUpperCase());
+    const order = await Order.findOrFail(id);
+
+    var discount,
+      info = {};
+
+    try {
+      const service = new Service(order);
+      const canAddDiscount = await service.canApplyDiscount(coupon);
+      const orderDiscounts = await order.coupons().getCount();
+
+      const canApplyToOrder =
+        orderDiscounts < 1 || (orderDiscounts >= 1 && coupon.recursive);
+      if (canAddDiscount && canApplyToOrder) {
+        discount = await Discount.findOrCreate({
+          order_id: order.id,
+          coupon_id: coupon.id,
+        });
+
+        info.message = 'Cupom aplicado com sucesso!';
+        info.success = true;
+      } else {
+        info.message = 'Não foi possível aplicar este cupom!';
+        info.success = false;
+      }
+
+      return response.send({ order, info });
+    } catch (error) {
+      return response.status(400).send({ message: 'Erro ao aplicar cupom!' });
     }
   }
 }
