78662e0f349baba995b5f6e0da55561696d89db0
brenu <bvsousa64@gmail.com>
2020-07-22 00:52:36 -0300

    :construction: Set error messages for register validator

--- app/Validators/Auth/Register.js
+++ app/Validators/Auth/Register.js
@@ -10,6 +10,18 @@ class Register {
       password: 'required|confirmed|min:8',
     };
   }
+
+  get messages() {
+    return {
+      'name.required': 'O nome é obrigatório!',
+      'surname.required': 'O sobrenome é obrigatório!',
+      'email.required': 'O E-Mail é obrigatório!',
+      'email.email': 'E-Mail inválido!',
+      'email.unique': 'O E-Mail já está em uso!',
+      'password.required': 'A senha é obrigatória!',
+      'password.confirmed': 'As senhas não são iguais!',
+    };
+  }
 }
 
 module.exports = Register;
