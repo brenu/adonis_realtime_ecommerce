a9253710fd35275534895dfae3ca0ec17e060920
brenu <bvsousa64@gmail.com>
2020-07-23 08:36:32 -0300

    :construction: Add category transformer to category controller

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -43,11 +43,13 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {
+  async store({ request, response, transform }) {
     try {
       const { title, description, image_id } = request.all();
 
-      const category = await Category.create({ title, description, image_id });
+      var category = await Category.create({ title, description, image_id });
+
+      category = await transform.item(category, Transformer);
 
       return response.status(201).send(category);
     } catch (error) {
@@ -66,8 +68,9 @@ class CategoryController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params: { id }, request, response, view }) {
-    const category = await Category.findOrFail(id);
+  async show({ params: { id }, transform, response }) {
+    var category = await Category.findOrFail(id);
+    category = await transform.item(category, Transformer);
 
     return response.send(category);
   }
@@ -79,13 +82,14 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params: { id }, request, response }) {
-    const category = await Category.findOrFail(id);
+  async update({ params: { id }, request, response, transform }) {
+    var category = await Category.findOrFail(id);
     const { title, description, image_id } = request.all();
 
     category.merge({ title, description, image_id });
 
     await category.save();
+    category = await transform.item(category, Transformer);
     return response.send(category);
   }
 
